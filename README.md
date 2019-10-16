7b9ef883030299dffa01d9bce63f2b6c4aa2b26f
hrokling <hrokling@hotmail.com>
2019-10-17 00:15:55 +0200

    Initial commit of script
    - Added main loop

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # blocker
 **Web host blocker for Mac users**
+
 My version of a web hostname blocker, which daily stops access to the hostnames added in the host name list.
 
 Version: Python 3
--- blocker.py
+++ blocker.py
@@ -1,1 +1,31 @@
-# This is where the code will go
+import time
+import datetime
+
+path = "/etc/hosts"
+local = "127.0.0.1"
+hostnamelist = ["www.facebook.com", "facebook.com", "www.vg.no", "vg.no"]
+
+while True:
+    now = datetime.datetime.now()  # Get the current time
+    # Blocking starts at 09.00
+    blocktimestart = now.replace(hour=9, minute=0, second=0, microsecond=0)
+    # Blocking ends at 17.00
+    blocktimeend = now.replace(hour=17, minute=0, second=0, microsecond=0)
+
+    if now > blocktimestart and now < blocktimeend:
+        with open((path), "r+") as file:
+            content = file.read()
+            for hostname in hostnamelist:
+                if hostname in content:
+                    pass
+                else:
+                    file.write(local + " " + hostname + "\n")
+    else:
+        with open((path), "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(hostname in line for hostname in hostnamelist):
+                    file.write(line)
+            file.truncate()
+    time.sleep(900)
